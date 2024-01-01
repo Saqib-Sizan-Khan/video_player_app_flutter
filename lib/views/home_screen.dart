41851c98e5aafdae117f2f91d5026b6579fcc356
Saqib Sizan Khan <77047241+Saqib-Sizan-Khan@users.noreply.github.com>
2024-01-02 00:08:29 +0600

    Next Page feature added into Home Screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -25,100 +25,132 @@ class HomeScreen extends ConsumerWidget {
                 final videos = ref.watch(videoProvider);
                 return videos.when(
                     data: (videoData) {
-                      return ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: videoData.length,
-                          itemBuilder: (context, index) {
-                            String videoDate = DateFormat('MMM dd, yyyy')
-                                .format(videoData[index].dateAndTime);
-                            return InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => PlayVideoScreen(
-                                        video: videoData[index]),
-                                  ),
-                                );
-                              },
-                              child: Column(
-                                children: [
-                                  Container(
-                                    width: 346.w,
-                                    height: 192.h,
-                                    decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                            image: NetworkImage(
-                                                videoData[index].thumbnail),
-                                            fit: BoxFit.cover)),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(12),
-                                      child: Align(
-                                        alignment: Alignment.bottomRight,
-                                        child: Container(
-                                          width: 35,
-                                          height: 17,
-                                          decoration: BoxDecoration(
-                                              color: Colors.black,
-                                              borderRadius:
-                                                  BorderRadius.circular(4)),
-                                          child: Center(
-                                              child: TextWidget(
-                                                  text:
-                                                      videoData[index].duration,
-                                                  textColor: Colors.white,
-                                                  fontSize: 12,
-                                                  fontWeight: FontWeight.w500)),
+                      return Column(
+                        children: [
+                          ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: videoData.length,
+                              itemBuilder: (context, index) {
+                                String videoDate = DateFormat('MMM dd, yyyy')
+                                    .format(videoData[index].dateAndTime);
+                                return InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => PlayVideoScreen(
+                                            video: videoData[index]),
+                                      ),
+                                    );
+                                  },
+                                  child: Column(
+                                    children: [
+                                      Container(
+                                        width: 346.w,
+                                        height: 192.h,
+                                        decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                                image: NetworkImage(
+                                                    videoData[index].thumbnail),
+                                                fit: BoxFit.cover)),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(12),
+                                          child: Align(
+                                            alignment: Alignment.bottomRight,
+                                            child: Container(
+                                              width: 35,
+                                              height: 17,
+                                              decoration: BoxDecoration(
+                                                  color: Colors.black,
+                                                  borderRadius:
+                                                      BorderRadius.circular(4)),
+                                              child: Center(
+                                                  child: TextWidget(
+                                                      text:
+                                                          videoData[index].duration,
+                                                      textColor: Colors.white,
+                                                      fontSize: 12,
+                                                      fontWeight: FontWeight.w500)),
+                                            ),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                  ),
-                                  SizedBox(height: 16.h),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 12),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        CircleAvatar(
-                                          radius: 20.spMax,
-                                          backgroundImage: NetworkImage(
-                                              videoData[index].channelImage),
-                                        ),
-                                        Column(
+                                      SizedBox(height: 16.h),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 12),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
                                           crossAxisAlignment:
                                               CrossAxisAlignment.start,
                                           children: [
-                                            SizedBox(
-                                              width: 235.w,
-                                              child: TextWidget(
-                                                  text: videoData[index].title,
-                                                  fontSize: 15,
-                                                  fontWeight: FontWeight.w600),
+                                            CircleAvatar(
+                                              radius: 20.spMax,
+                                              backgroundImage: NetworkImage(
+                                                  videoData[index].channelImage),
                                             ),
-                                            SizedBox(height: 8.h),
-                                            TextWidget(
-                                                text:
-                                                    '${videoData[index].viewers} views  .  $videoDate',
-                                                textColor: Colors.grey,
-                                                fontSize: 13,
-                                                fontWeight: FontWeight.w400)
+                                            Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                SizedBox(
+                                                  width: 235.w,
+                                                  child: TextWidget(
+                                                      text: videoData[index].title,
+                                                      fontSize: 15,
+                                                      fontWeight: FontWeight.w600),
+                                                ),
+                                                SizedBox(height: 8.h),
+                                                TextWidget(
+                                                    text:
+                                                        '${videoData[index].viewers} views  .  $videoDate',
+                                                    textColor: Colors.grey,
+                                                    fontSize: 13,
+                                                    fontWeight: FontWeight.w400)
+                                              ],
+                                            ),
+                                            const Icon(Icons.more_vert_outlined,
+                                                color: Colors.grey)
                                           ],
                                         ),
-                                        const Icon(Icons.more_vert_outlined,
-                                            color: Colors.grey)
-                                      ],
-                                    ),
+                                      ),
+                                      SizedBox(height: 18.h)
+                                    ],
                                   ),
-                                  SizedBox(height: 18.h)
-                                ],
-                              ),
-                            );
-                          });
+                                );
+                              }),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: SizedBox(
+                              width: 100.w,
+                              height: 40.h,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  ref.refresh(videoProvider);
+                                },
+                                style: ElevatedButton.styleFrom(
+                                    primary: Colors.grey.shade50,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(12)),
+                                    side: const BorderSide(width: 1, color: Colors.grey)),
+                                child: Row(
+                                  children: [
+                                    TextWidget(
+                                        text: 'Next',
+                                        textColor: Colors.indigoAccent,
+                                        fontSize: 14.sp,
+                                        fontWeight: FontWeight.w700),
+                                    SizedBox(width: 5.w),
+                                    const Icon(Icons.arrow_forward_ios,
+                                        color: Colors.indigoAccent, size: 18),
+                                  ],
+                                ),),
+                            ),
+                          )
+                        ],
+                      );
                     },
                     error: (error, stackTrace) =>
                         Center(child: Text('Error: $error')),
